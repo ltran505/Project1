4480b68a8627ad50aec019d1e87e8b91cd2f1619
ltran505 <ltran505@student.cccd.edu>
2024-05-08 15:22:07 -0700

    copy cons

--- Project1/CookieList.cpp
+++ Project1/CookieList.cpp
@@ -130,6 +130,28 @@ CookieList::CookieList(const CookieList& other)
 		addCookie(cookie.getName(), cookie.getCalories(), cookie.getServings(), cookie.getIngredients());
 		current = current->getNext();
 	}
+
+	CookieList::CookieList(const CookieList & other)
+	{
+
+		if (this->isEmpty())
+		{
+			clearList();
+		}
+
+		Node* current = other.first;
+		first = current;
+		while (current != nullptr)
+		{
+			const Cookie& aCookie = current->getCookie();
+			this->addCookie(aCookie);
+			current = current->getNext();
+		}
+		count = other.count;
+		Node* newLast = other.last;
+		last = newLast;
+	}
+
 }
 
 
